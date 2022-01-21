a9e27f30b9b145c4379800bf320ee4f4c7fb2314
Ruben de Roos <deroosruben@gmail.com>
2022-01-21 16:15:36 +0100

    change to set

--- src/main/java/nl/thedutchruben/mccore/utils/classes/ClassFinder.java
+++ src/main/java/nl/thedutchruben/mccore/utils/classes/ClassFinder.java
@@ -5,15 +5,14 @@ import com.google.common.reflect.ClassPath;
 import lombok.SneakyThrows;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Set;
 
 public class ClassFinder {
 
     @SneakyThrows
-    public List<Class<?>> findClasses(String path) throws IOException {
-        List<Class<?>> classes = new ArrayList<>();
+    public Set<Class<?>> findClasses(String path) {
+        Set<Class<?>> classes = new HashSet<>();
         ClassLoader cl = getClass().getClassLoader();
         Set<ClassPath.ClassInfo> classesInPackage = ClassPath.from(cl).getTopLevelClassesRecursive(path);
         for (ClassPath.ClassInfo classInfo : classesInPackage) {
