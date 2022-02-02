fbc20e1dc8fd77e107fb444790ec34545222b262
Ruben de Roos <deroosruben@gmail.com>
2022-02-02 20:21:05 +0100

    change line drawing

--- src/main/java/nl/thedutchruben/mccore/utils/particle/ParticleUtil.java
+++ src/main/java/nl/thedutchruben/mccore/utils/particle/ParticleUtil.java
@@ -4,23 +4,22 @@ package nl.thedutchruben.mccore.utils.particle;
 import lombok.experimental.UtilityClass;
 import org.bukkit.Location;
 import org.bukkit.Particle;
+import org.bukkit.World;
 import org.bukkit.util.Vector;
 
 @UtilityClass
 public class ParticleUtil {
 
-    public void line(Location start, Location end, Particle particle) {
-
-        Vector dir = end.getDirection().multiply(2);
-
-        for (double i = 0; i < 10; i += 0.5) {
-            dir.multiply(i);
-            start.add(dir);
-
-            start.getWorld().spawnParticle(particle,start,1);
-
-            start.subtract(dir);
-            dir.normalize();
+    public void drawLine(Location point1, Location point2, double space, Particle particle) {
+        World world = point1.getWorld();
+        double distance = point1.distance(point2);
+        Vector p1 = point1.toVector();
+        Vector p2 = point2.toVector();
+        Vector vector = p2.clone().subtract(p1).normalize().multiply(space);
+        double length = 0;
+        for (; length < distance; p1.add(vector)) {
+            world.spawnParticle(particle, p1.getX(), p1.getY(), p1.getZ(), 1);
+            length += space;
         }
     }
 }
