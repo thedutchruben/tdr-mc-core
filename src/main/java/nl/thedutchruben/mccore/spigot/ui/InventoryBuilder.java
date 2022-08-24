986d82edefadd2f5f622900b359d89e328f94f1c
Ruben de Roos <deroosruben@gmail.com>
2022-08-24 22:03:37 +0200

    add holder

--- src/main/java/nl/thedutchruben/mccore/spigot/ui/InventoryBuilder.java
+++ src/main/java/nl/thedutchruben/mccore/spigot/ui/InventoryBuilder.java
@@ -1,17 +1,37 @@
 package nl.thedutchruben.mccore.spigot.ui;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 
 public class InventoryBuilder {
     private String title;
     private InventoryType inventoryType;
+
+    private InventoryHolder inventoryHolder;
+    
+
     public InventoryBuilder(String title) {
-        this.title = title
+        this.title = title;
     }
 
     public InventoryBuilder type(InventoryType type){
         inventoryType = type;
         return this;
+    }
+
+    public InventoryBuilder holder(InventoryHolder holder){
+        inventoryHolder = holder;
+        return this;
+    }
+
+
+
+    public Inventory build(){
+        Inventory inventory = Bukkit.createInventory(inventoryHolder, inventoryType, title);
+
+        return inventory;
     }
 
 }
