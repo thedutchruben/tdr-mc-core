6d005186d7f382b3467520e13c8d611b7934b1ef
Ruben de Roos <deroosruben@gmail.com>
2023-09-08 20:44:00 +0200

    Add hex option

--- src/main/java/nl/thedutchruben/mccore/utils/message/MessageUtil.java
+++ src/main/java/nl/thedutchruben/mccore/utils/message/MessageUtil.java
@@ -5,8 +5,15 @@ import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.hover.content.Text;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static net.md_5.bungee.api.ChatColor.COLOR_CHAR;
+
 
 @UtilityClass
 public class MessageUtil {
@@ -44,6 +51,29 @@ public class MessageUtil {
         component.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
                 new Text(ChatColor.translateAlternateColorCodes('&', description))));
         return component;
+    }
+
+    public String translateHexColorCodes(String startTag, String endTag, String message)
+    {
+        String[] split = Bukkit.getBukkitVersion().split("-")[0].split("\\.");
+        if(Integer.parseInt(split[1]) >= 16) {
+            final Pattern hexPattern = Pattern.compile(startTag + "#?([A-Fa-f0-9]{6})" + endTag);
+            Matcher matcher = hexPattern.matcher(message);
+            StringBuffer buffer = new StringBuffer(message.length() + 4 * 8);
+            while (matcher.find())
+            {
+                String group = matcher.group(1).replace("#", "");
+                matcher.appendReplacement(buffer, COLOR_CHAR + "x"
+                        + COLOR_CHAR + group.charAt(0) + COLOR_CHAR + group.charAt(1)
+                        + COLOR_CHAR + group.charAt(2) + COLOR_CHAR + group.charAt(3)
+                        + COLOR_CHAR + group.charAt(4) + COLOR_CHAR + group.charAt(5)
+                );
+            }
+            return matcher.appendTail(buffer).toString();
+        }else{
+            return message;
+        }
+
     }
 
 }
